f3c489c858ab3357594252306666028595f1517a
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-04-03 19:55:12 -0400

    connection update

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 const sequelize = new Sequelize(
-process.env.MYSQLDATABASE,
-process.env.MYSQLUSER,
-process.env.MYSQLPASSWORD,
+process.env.DB_NAME,
+process.env.DB_USER,
+process.env.DB_PASSWORD,
 {
   host: process.env.MYSQLHOST,
   dialect: 'mysql',
