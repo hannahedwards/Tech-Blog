55270107f7b89e17cb706f7943613735cf10a58c
Hannah Edwards <hannahedwards@MacBook-Pro-2.local>
2023-04-03 23:52:34 -0400

    server update

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const ip = '0.0.0.0'
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -43,5 +42,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, ip, () => console.log('Now listening on http://localhost:3001/'));
+  app.listen(PORT, () => console.log('Now listening on http://localhost:3001/'));
 });
